21809e5728fe007142ec0716a2d13cb86dee783e
nemanja731 <nemanja.jankovic731@gmail.com>
2023-09-13 06:40:53 +0200

    Update position of dataset

--- classification.py
+++ classification.py
@@ -202,6 +202,220 @@ def choose_attributes(dataset):
     # information_gain(k, X_old, y_old)
     # rfe_(k, X_old, y_old)
 
+    # dataset = dataset[
+    #     [
+    #         "diagnosis",
+    #         "area_se",
+    #         "radius_worst",
+    #         "concave points_worst",
+    #         "concavity_mean",
+    #         "symmetry_worst",
+    #         "texture_worst",
+    #     ]
+    # ]
+
+    new_corr = dataset.corr()
+    # print(
+    #     "\nIzdvojeni prediktori sa odgovarajucim koeficijentom korelacije sa izlazom: \n\n",
+    #     new_corr["diagnosis"],
+    # )
+    return dataset
+
+
+# treniranje i klasifikacija
+def train_and_classify(dataset, classifier, name, filename):
+    X = dataset.drop(columns=["diagnosis"])
+    y = dataset["diagnosis"]
+    _, num_attributes = dataset.shape
+    # 1/5 = 20% test, 4/5 = 80% training
+    num_splits = 5
+    folds = StratifiedKFold(n_splits=num_splits)
+    scores_accuracy = []
+    scores_precision = []
+    scores_f1_score = []
+    scores_recall = []
+    i = 1
+
+    for train_index, test_index in folds.split(X, y):
+        prediction, y_test = train_and_classify_one_model(
+            classifier, train_index, test_index, X, y
+        )
+        # make_report(classifier, prediction, y_test, name, filename, i)
+        scores_accuracy.append(accuracy_score(prediction, y_test))
+        scores_precision.append(precision_score(prediction, y_test))
+        scores_f1_score.append(f1_score(prediction, y_test))
+        scores_recall.append(recall_score(prediction, y_test))
+        i += 1
+
+    reportForAllModels(
+        name,
+        num_attributes - 1,
+        num_splits,
+        scores_accuracy,
+        scores_precision,
+        scores_recall,
+        scores_f1_score,
+    )
+
+# treniranje i klasifikacija za 1 iteraciju 1 modela
+def train_and_classify_one_model(classifier, train_index, test_index, X, y):
+    X_train, X_test, y_train, y_test = (
+        X.iloc[train_index, :],
+        X.iloc[test_index, :],
+        y.iloc[train_index],
+        y.iloc[test_index],
+    )
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+    classifier.fit(X_train, y_train)
+    prediction = classifier.predict(X_test)
+    return prediction, y_test
+
+
+# pravljenje izvestaja za 1 iteraciju 1 modela
+def make_report(classifier, prediction, y_test, name, filename, i):
+    if (i != 1):
+        print("\n-----------------------\n")
+    print(str(i) + ". Klasifikator:\n")
+    print(
+        classification_report(
+            y_test, prediction, target_names=["Maligna masa: 0", "Benigna masa: 1"]
+        )
+    )
+    print(
+        "Tacnost modela iznosi: {:.2f}%".format(
+            accuracy_score(y_test, prediction) * 100
+        )
+    )
+    print(
+        "Preciznost modela iznosi: {:.2f}%".format(
+            precision_score(y_test, prediction) * 100
+        )
+    )
+    print(
+        "Recall modela iznosi: {:.2f}%".format(recall_score(y_test, prediction) * 100)
+    )
+    print(
+        "Balansirana tacnost iznosi: {:.2f}%".format(f1_score(y_test, prediction) * 100)
+    )
+    plot_confusion_matrix(y_test, prediction, i, name, filename)
+    if (filename == "dt"):
+        plot_tree(classifier, i)
+
+
+# pravljenje konacnog izvestaja za 1 model
+def reportForAllModels(
+    name,
+    num_attributes,
+    num_splits,
+    scores_accuracy,
+    scores_precision,
+    scores_recall,
+    scores_f1_score,
+):
+    print(
+        "\nIzabrano je {} atributa pri analizi, a rezultati su sledeci:\n".format(
+            num_attributes
+        )
+    )
+    print(
+        "Usrednjena tacnost za "
+        + str(num_splits)
+        + " modela za " + name +  " iznosi: {:.2f}%".format(100 * np.average(scores_accuracy))
+    )
+    print(
+        "Usrednjena preciznost za "
+        + str(num_splits)
+        + " modela  " + name +  " iznosi: {:.2f}%".format(100 * np.average(scores_precision))
+    )
+    print(
+        "Usrednjen recall za "
+        + str(num_splits)
+        + " modela  " + name +  " iznosi: {:.2f}%".format(100 * np.average(scores_recall))
+    )
+    print(
+        "Usrednjen f1 score za "
+        + str(num_splits)
+        + " modela  " + name +  " iznosi: {:.2f}%".format(100 * np.average(scores_f1_score))
+    )
+
+
+# prikaz konfuzione matrice
+def plot_confusion_matrix(y_test, prediction, i, name, filename):
+    confusion_matrix = metrics.confusion_matrix(y_test, prediction)
+    cm_display = ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [False, True])
+    cm_display.plot()
+    plt.savefig('./images/conf_mat_' + filename + str(i) + '.png')
+    plt.title("Konfuziona matrica - " + name + " " + str(i), weight="bold")
+    plt.xlabel("Predikcija")
+    plt.ylabel("Stvarna vrednost")
+    plt.show()
+    print('Tacnost modela ' + name + ' iznosi: {:.3f}%'.format(accuracy_score(y_test,prediction)*100))
+    print('Balansirana tacnost modela ' + name + ' iznosi: {:.3f}%'.format(f1_score(y_test,prediction)*100))
+
+
+# prikaz stabla
+def plot_tree(classifier, i):
+    feature_names = [l for l in list(dataset.columns)[1:]]
+    class_names = ["Benigni", "Maligni"]
+    fig = plt.figure(figsize=(30, 20))
+    _ = tree.plot_tree(
+        classifier,
+        feature_names=feature_names,
+        class_names=class_names,
+        filled=True,
+        fontsize=8,
+    )
+    plt.title("Prikaz " + str(i) + ". stabla odlucivanja", weight="bold")
+    plt.savefig("./images/tree" + str(i) + ".png")
+    plt.show()
+
+
+def decision_tree(dataset):
+    print("\n-----------------------\n")
+    print("Stablo odlucivanja:\n")
+    classifier = DecisionTreeClassifier()
+    train_and_classify(dataset, classifier, "Stablo odlucivanja", "dt") 
+
+
+def random_forest(dataset):
+    print("\n-----------------------\n")
+    print("Slucajne sume:\n")
+    classifier = RandomForestClassifier(n_estimators=50)
+    train_and_classify(dataset, classifier, "Slucajne sume", "rf")
+
+
+def XGBoost(dataset):
+    print("\n-----------------------\n")
+    print("XGBoost:\n")
+    classifier = XGBClassifier(n_estimators=350, subsample=0.8, max_depth=7, eval_metric = 'logloss')
+    train_and_classify(dataset, classifier, "XGBoost", "xgb")
+
+
+def AdaBoost(dataset):
+    print("\n-----------------------\n")
+    print("AdaBoost:\n")
+    classifier = AdaBoostClassifier(n_estimators=50, learning_rate=1)
+    train_and_classify(dataset, classifier, "AdaBoost", "adb")
+
+
+def logistic_regression(dataset):
+    print("\n-----------------------\n")
+    print("Logisticka regresija:\n")
+    classifier = LogisticRegression()
+    train_and_classify(dataset, classifier, "Logisticka regresija", "lr")
+
+
+def naive_bayes(dataset):
+    print("\n-----------------------\n")
+    print("Naivni Bajes:\n")
+    classifier = GaussianNB()
+    train_and_classify(dataset, classifier, "Naivnog Bajesa", "nb")
+    print("\n-----------------------\n")
+
+# poziva treniranje i klasifikaciju za svaki model
+def classify(dataset):
     dataset = dataset[
         [
             "diagnosis",
@@ -213,13 +427,20 @@ def choose_attributes(dataset):
             "texture_worst",
         ]
     ]
+    decision_tree(dataset)
+    random_forest(dataset)
+    XGBoost(dataset)
+    AdaBoost(dataset)
+    logistic_regression(dataset)
+    naive_bayes(dataset)
 
-    new_corr = dataset.corr()
-    # print(
-    #     "\nIzdvojeni prediktori sa odgovarajucim koeficijentom korelacije sa izlazom: \n\n",
-    #     new_corr["diagnosis"],
-    # )
-    return dataset
+if __name__ == "__main__":
+    # analiziranje klasa i atributa
+    dataset = analyze_base()
+    # izbor atributa
+    dataset = choose_attributes(dataset)
+    # podela skupa podataka, treniranje i klasifikacija razlicitim modelima
+    classify(dataset)
 
     # dataset = dataset[
     #     [
@@ -305,226 +526,3 @@ def choose_attributes(dataset):
     #         "concave points_mean",
     #     ]
     # ]
-
-
-# treniranje i klasifikacija
-def train_and_classify(dataset, classifier, name, filename):
-    X = dataset.drop(columns=["diagnosis"])
-    y = dataset["diagnosis"]
-    _, num_attributes = dataset.shape
-    # 1/5 = 20% test, 4/5 = 80% training
-    num_splits = 5
-    folds = StratifiedKFold(n_splits=num_splits)
-    scores_accuracy = []
-    scores_precision = []
-    scores_f1_score = []
-    scores_recall = []
-    i = 1
-
-    for train_index, test_index in folds.split(X, y):
-        prediction, y_test = train_and_classify_one_model(
-            classifier, train_index, test_index, X, y
-        )
-        # make_report(prediction, y_test, name, filename, i)
-        scores_accuracy.append(accuracy_score(prediction, y_test))
-        scores_precision.append(precision_score(prediction, y_test))
-        scores_f1_score.append(f1_score(prediction, y_test))
-        scores_recall.append(recall_score(prediction, y_test))
-        i += 1
-
-    reportForAllModels(
-        name,
-        num_attributes - 1,
-        num_splits,
-        scores_accuracy,
-        scores_precision,
-        scores_recall,
-        scores_f1_score,
-    )
-
-
-def train_and_classify_one_model(classifier, train_index, test_index, X, y):
-    X_train, X_test, y_train, y_test = (
-        X.iloc[train_index, :],
-        X.iloc[test_index, :],
-        y.iloc[train_index],
-        y.iloc[test_index],
-    )
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
-    classifier = DecisionTreeClassifier(max_depth=5, criterion="entropy")
-    classifier.fit(X_train, y_train)
-    prediction = classifier.predict(X_test)
-    return prediction, y_test
-
-
-# pravljenje izvestaja za 1 stablo odluke u toku procesa
-def make_report(prediction, y_test, name, filename, i):
-    if (i != 1):
-        print("\n-----------------------\n")
-    print(str(i) + ". Klasifikator:\n")
-    print(
-        classification_report(
-            y_test, prediction, target_names=["Maligna masa: 0", "Benigna masa: 1"]
-        )
-    )
-    print(
-        "Tacnost modela iznosi: {:.2f}%".format(
-            accuracy_score(y_test, prediction) * 100
-        )
-    )
-    print(
-        "Preciznost modela iznosi: {:.2f}%".format(
-            precision_score(y_test, prediction) * 100
-        )
-    )
-    print(
-        "Recall modela iznosi: {:.2f}%".format(recall_score(y_test, prediction) * 100)
-    )
-    print(
-        "Balansirana tacnost iznosi: {:.2f}%".format(f1_score(y_test, prediction) * 100)
-    )
-    # plot_confusion_matrix(y_test, prediction, i, name, filename)
-    # if (filename == "dt")
-    #   plot_tree(classifier, i)
-
-
-# pravljenje krajnjeg izvestaja
-def reportForAllModels(
-    name,
-    num_attributes,
-    num_splits,
-    scores_accuracy,
-    scores_precision,
-    scores_recall,
-    scores_f1_score,
-):
-    print(
-        "\nIzabrano je {} atributa pri analizi, a rezultati su sledeci:\n".format(
-            num_attributes
-        )
-    )
-    print(
-        "Usrednjena tacnost za "
-        + str(num_splits)
-        + " modela za " + name +  " iznosi: {:.2f}%".format(100 * np.average(scores_accuracy))
-    )
-    print(
-        "Usrednjena preciznost za "
-        + str(num_splits)
-        + " modela  " + name +  " iznosi: {:.2f}%".format(100 * np.average(scores_precision))
-    )
-    print(
-        "Usrednjen recall za "
-        + str(num_splits)
-        + " modela  " + name +  " iznosi: {:.2f}%".format(100 * np.average(scores_recall))
-    )
-    print(
-        "Usrednjen f1 score za "
-        + str(num_splits)
-        + " modela  " + name +  " iznosi: {:.2f}%".format(100 * np.average(scores_f1_score))
-    )
-
-
-# prikaz konfuzione matrice
-def plot_confusion_matrix(y_test, prediction, i, name, filename):
-    confusion_matrix = metrics.confusion_matrix(y_test, prediction)
-    cm_display = ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [False, True])
-    cm_display.plot()
-    plt.savefig('./images/conf_mat_' + filename + str(i) + '.png')
-    plt.title("Konfuziona matrica - " + name + " " + str(i), weight="bold")
-    plt.xlabel("Predikcija")
-    plt.ylabel("Stvarna vrednost")
-    plt.show()
-    print('Tacnost modela ' + name + ' iznosi: {:.3f}%'.format(accuracy_score(y_test,prediction)*100))
-    print('Balansirana tacnost modela ' + name + ' iznosi: {:.3f}%'.format(f1_score(y_test,prediction)*100))
-
-
-# prikaz stabla
-def plot_tree(classifier, i):
-    feature_names = [l for l in list(dataset.columns)[1:]]
-    class_names = ["Benigni", "Maligni"]
-    fig = plt.figure(figsize=(30, 20))
-    _ = tree.plot_tree(
-        classifier,
-        feature_names=feature_names,
-        class_names=class_names,
-        filled=True,
-        fontsize=8,
-    )
-    plt.title("Prikaz " + str(i) + ". stabla odlucivanja", weight="bold")
-    plt.savefig("./images/tree" + str(i) + ".png")
-    plt.show()
-
-
-def classify(dataset):
-    dataset = dataset[
-        [
-            "diagnosis",
-            "area_se",
-            "radius_worst",
-            "concave points_worst",
-            "concavity_mean",
-            "symmetry_worst",
-            "texture_worst",
-        ]
-    ]
-    decision_tree(dataset)
-    random_forest(dataset)
-    XGBoost(dataset)
-    AdaBoost(dataset)
-    logistic_regression(dataset)
-    naive_bayes(dataset)
-
-
-def decision_tree(dataset):
-    print("\n-----------------------\n")
-    print("Stablo odlucivanja:\n")
-    classifier = DecisionTreeClassifier()
-    train_and_classify(dataset, classifier, "Stablo odlucivanja", "dt") 
-
-
-def random_forest(dataset):
-    print("\n-----------------------\n")
-    print("Slucajne sume:\n")
-    classifier = RandomForestClassifier()
-    train_and_classify(dataset, classifier, "Slucajne sume", "rf")
-
-
-def XGBoost(dataset):
-    print("\n-----------------------\n")
-    print("XGBoost:\n")
-    classifier = XGBClassifier(n_estimators=350, subsample=0.8, max_depth=7, eval_metric = 'logloss')
-    train_and_classify(dataset, classifier, "XGBoost", "xgb")
-
-
-def AdaBoost(dataset):
-    print("\n-----------------------\n")
-    print("AdaBoost:\n")
-    classifier = AdaBoostClassifier(n_estimators=50, learning_rate=1)
-    train_and_classify(dataset, classifier, "AdaBoost", "adb")
-
-
-def logistic_regression(dataset):
-    print("\n-----------------------\n")
-    print("Logisticka regresija:\n")
-    classifier = LogisticRegression()
-    train_and_classify(dataset, classifier, "Logisticka regresija", "lr")
-
-
-def naive_bayes(dataset):
-    print("\n-----------------------\n")
-    print("Naivni Bajes:\n")
-    classifier = GaussianNB()
-    train_and_classify(dataset, classifier, "Naivnog Bajesa", "nb")
-    print("\n-----------------------\n")
-
-
-if __name__ == "__main__":
-    # analiziranje klasa i atributa
-    dataset = analyze_base()
-    # izbor atributa
-    dataset = choose_attributes(dataset)
-    # podela skupa podataka, treniranje i klasifikacija razlicitim metodama
-    classify(dataset)
